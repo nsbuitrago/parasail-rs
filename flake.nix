9063c00fe9c54470ad6dcf5972b89b32096dac30
nsbuitrago <44626938+nsbuitrago@users.noreply.github.com>
2025-06-06 12:13:16 -0500

    add: nix flake

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "parasail-rs dev environment";
+
+  inputs = {
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: inputs.nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = [
+            inputs.rust-overlay.overlays.default
+            inputs.self.overlays.default
+          ];
+        };
+      });
+    in
+    {
+      overlays.default = final: prev: {
+        rustToolchain = final.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+      };
+
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            rustToolchain
+            openssl
+            pkg-config
+            cargo-deny
+            cargo-edit
+            cargo-watch
+            rust-analyzer
+            cmake
+            git
+          ];
+
+          env = {
+            # Required by rust-analyzer
+            RUST_SRC_PATH = "${pkgs.rustToolchain}/lib/rustlib/src/rust/library";
+          };
+        };
+      });
+    };
+}
